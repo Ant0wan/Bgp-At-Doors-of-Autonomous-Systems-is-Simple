8697d23f2e17e0ada06da5d8ca9144ca30fc2c99
Antoine Barthelemy <antoine@linux.com>
2022-10-22 17:54:00 +0200

    update

--- utils/gns3-install.sh
+++ utils/gns3-install.sh
@@ -13,6 +13,7 @@ cd build
 cmake ..
 sudo make install
 [[ "$(which dynamips)" == "/usr/local/bin/dynamips" ]]
+cd ../../
 
 wget https://liquidtelecom.dl.sourceforge.net/project/vpcs/0.8/vpcs_0.8b_Linux64
 mv vpcs_0.8b_Linux64 vpcs
