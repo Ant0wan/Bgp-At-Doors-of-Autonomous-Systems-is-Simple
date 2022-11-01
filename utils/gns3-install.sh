ee6d0b9949c73141c9286d982ab1ec25f1fe1ffc
Antoine Barthelemy <antoine@linux.com>
2022-11-01 19:52:09 +0100

    remove cd

--- utils/gns3-install.sh
+++ utils/gns3-install.sh
@@ -7,13 +7,12 @@ sudo dnf -y install wireshark
 sudo dnf -y install gns3-server gns3-gui
 
 git clone https://github.com/GNS3/dynamips
-cd dynamips ## should change CDs
+pushd dynamips
 mkdir build
-cd build
-cmake ..
+cmake -B build
 sudo make install
 [[ "$(which dynamips)" == "/usr/local/bin/dynamips" ]]
-cd ../../
+popd
 
 wget https://liquidtelecom.dl.sourceforge.net/project/vpcs/0.8/vpcs_0.8b_Linux64
 mv vpcs_0.8b_Linux64 vpcs
@@ -24,12 +23,12 @@ vpcs -v
 . kvm.sh
 
 git clone http://github.com/ndevilla/iniparser.git
-cd iniparser
+pushd iniparser
 make
 sudo cp libiniparser.* /usr/lib/
 sudo cp src/iniparser.h /usr/local/include
 sudo cp src/dictionary.h /usr/local/include
-cd ..
+popd
 
 . docker.sh
 
